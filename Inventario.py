b8afa7a36474a7d6eedba4575c2001cdff8c7b55
martinezgaby66304 <martinezgaby66304@gmail.com>
2025-11-07 15:37:02 -0600

    Instancia de variables y clase

---
+++ Inventario.py
@@ -0,0 +1,9 @@
+class Producto:
+    def __init__(self, K, D, h):
+        self.k = K
+        self.D = D
+        self.h = h
+K_Producto=input("Ingresa el Valor de k:  ")
+D_Producto=input("Ingresa el Valor de D:  ")
+h_Producto=input("Ingresa el Valor de h:  ")   
+Variables=Producto(K_Producto,D_Producto,h_Producto)    
