f8ef1ce02abc19d98f25265590d5c2f807d54ed1
martinezgaby66304 <martinezgaby66304@gmail.com>
2025-11-08 11:43:54 -0600

    Primera operacion

--- Inventario.py
+++ Inventario.py
@@ -1,9 +1,14 @@
+import math 
 class Producto:
     def __init__(self, K, D, h):
         self.k = K
         self.D = D
         self.h = h
-K_Producto=input("Ingresa el Valor de k:  ")
-D_Producto=input("Ingresa el Valor de D:  ")
-h_Producto=input("Ingresa el Valor de h:  ")   
-Variables=Producto(K_Producto,D_Producto,h_Producto)    
+K_CostoProducto = float(input("Ingresa el Valor de k:  "))
+D_Demanda= float(input("Ingresa el Valor de D:  "))
+h_CostoMantenimiento= float(input("Ingresa el Valor de h:  "))  
+Variables=Producto(K_CostoProducto,D_Demanda,h_CostoMantenimiento) 
+def Cantidad_de_Pedido(K, D, h):
+    Cantidad = math.sqrt(2 * K * D / h)
+    print("La cantidad del pedido es:   ", Cantidad)
+Cantidad_de_Pedido(Variables.k, Variables.D, Variables.h)   
