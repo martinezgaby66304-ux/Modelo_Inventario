60640913c3765e03bdd0587f9f43bcc9c2f9ea36
martinezgaby66304 <martinezgaby66304@gmail.com>
2025-11-09 20:51:19 -0600

    Tercera operacion,tiempo ocurrido del pedido

--- Inventario.py
+++ Inventario.py
@@ -1,13 +1,15 @@
 import math 
 class Producto:
-    def __init__(self, K, D, h):
+    def __init__(self, K, D, h, L):
         self.k = K
         self.D = D
         self.h = h
+        self.L = L
 K_CostoProducto = float(input("Ingresa el Valor de k:  "))
 D_Demanda= float(input("Ingresa el Valor de D:  "))
 h_CostoMantenimiento= float(input("Ingresa el Valor de h:  "))  
-Variables=Producto(K_CostoProducto,D_Demanda,h_CostoMantenimiento) 
+L_DiasPedido= float(input("Ingresa el numero de dias en que realizas un pedido:  "))  
+Variables=Producto(K_CostoProducto,D_Demanda,h_CostoMantenimiento,L_DiasPedido) 
 def Cantidad_de_Pedido(K, D, h):
     y= math.sqrt(2 * K * D / h)
     print("La cantidad del pedido es:   ", y)
@@ -15,6 +17,13 @@ def Cantidad_de_Pedido(K, D, h):
 cantidad_pedido = Cantidad_de_Pedido(Variables.k, Variables.D, Variables.h)  
 
 def Renovacion_de_pedido(y , D):
-    RenovacionInventario = (y / D)
-    print("El inventario se renueva aproximadamente:   ", RenovacionInventario)  
-Renovacion_de_pedido(cantidad_pedido, Variables.D)
+    To = (y / D)
+    print("El inventario se renueva aproximadamente:   ", To) 
+    return To
+Renovacion = Renovacion_de_pedido(cantidad_pedido, Variables.D)
+
+def Tiempo_ocurridos(L , To):
+    n = (L / To)
+    print("El tiempo de renovacion del pedido es de:   ", n)
+Tiempo_ocurridos(Variables.L, Renovacion)
+     
