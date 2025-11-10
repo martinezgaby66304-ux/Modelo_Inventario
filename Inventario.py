c329e77cc355b79a91dc96cbd15381a5586689bc
martinezgaby66304 <martinezgaby66304@gmail.com>
2025-11-09 18:08:46 -0600

    Segunda formula,renovacion de inventario

--- Inventario.py
+++ Inventario.py
@@ -9,6 +9,12 @@ D_Demanda= float(input("Ingresa el Valor de D:  "))
 h_CostoMantenimiento= float(input("Ingresa el Valor de h:  "))  
 Variables=Producto(K_CostoProducto,D_Demanda,h_CostoMantenimiento) 
 def Cantidad_de_Pedido(K, D, h):
-    Cantidad = math.sqrt(2 * K * D / h)
-    print("La cantidad del pedido es:   ", Cantidad)
-Cantidad_de_Pedido(Variables.k, Variables.D, Variables.h)   
+    y= math.sqrt(2 * K * D / h)
+    print("La cantidad del pedido es:   ", y)
+    return y
+cantidad_pedido = Cantidad_de_Pedido(Variables.k, Variables.D, Variables.h)  
+
+def Renovacion_de_pedido(y , D):
+    RenovacionInventario = (y / D)
+    print("El inventario se renueva aproximadamente:   ", RenovacionInventario)  
+Renovacion_de_pedido(cantidad_pedido, Variables.D)
