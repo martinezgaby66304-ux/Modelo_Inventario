ff23e90ca1a14e8dad1a051a591dc843e5669c3f
martinezgaby66304 <martinezgaby66304@gmail.com>
2025-11-11 18:22:42 -0600

    cuarta y Quinta formula

--- Inventario.py
+++ Inventario.py
@@ -5,11 +5,11 @@ class Producto:
         self.D = D
         self.h = h
         self.L = L
-K_CostoProducto = float(input("Ingresa el Valor de k:  "))
-D_Demanda= float(input("Ingresa el Valor de D:  "))
-h_CostoMantenimiento= float(input("Ingresa el Valor de h:  "))  
-L_DiasPedido= float(input("Ingresa el numero de dias en que realizas un pedido:  "))  
-Variables=Producto(K_CostoProducto,D_Demanda,h_CostoMantenimiento,L_DiasPedido) 
+K_costo_producto = float(input("Ingresa el Valor de k:  "))
+D_demanda= float(input("Ingresa el Valor de D:  "))
+h_costo_mantenimiento= float(input("Ingresa el Valor de h:  "))  
+L_dias_pedido= float(input("Ingresa el numero de dias en que realizas un pedido:  "))  
+Variables=Producto(K_costo_producto, D_demanda, h_costo_mantenimiento, L_dias_pedido ) 
 def Cantidad_de_Pedido(K, D, h):
     y= math.sqrt(2 * K * D / h)
     print("La cantidad del pedido es:   ", y)
@@ -23,7 +23,22 @@ def Renovacion_de_pedido(y , D):
 Renovacion = Renovacion_de_pedido(cantidad_pedido, Variables.D)
 
 def Tiempo_ocurridos(L , To):
-    n = (L / To)
-    print("El tiempo de renovacion del pedido es de:   ", n)
-Tiempo_ocurridos(Variables.L, Renovacion)
-     
+    n = L / To
+    n_redondeado = round(n)
+    print("El numero de tiempos ocurridos es de:   ", n)
+    return n_redondeado
+Tiempo = Tiempo_ocurridos(Variables.L, Renovacion)
+
+def Inventario_restante(L, n, To):
+    Le = L - (n * To)
+    print("El inventario restante es de:   ", Le)
+    return Le
+Inventario = Inventario_restante(Variables.L, Tiempo, Renovacion)
+
+def Unidades_en_inventario(Le, D):
+    U = Le *  D
+    print("Cantidad de unidades que quedan en el inventario son :   ", U)
+    return U
+Unidades_en_inventario(Inventario, Variables.D)
+
+}
